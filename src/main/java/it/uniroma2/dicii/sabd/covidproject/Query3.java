f60315b34199c3ef0bba7a525e4a614f1fad863c
redefik <fe.viglietta@gmail.com>
2020-05-11 19:00:28 +0200

    Query 3: naive K-means

--- src/main/java/it/uniroma2/dicii/sabd/covidproject/Query3.java
+++ src/main/java/it/uniroma2/dicii/sabd/covidproject/Query3.java
@@ -6,6 +6,7 @@ import it.uniroma2.dicii.sabd.covidproject.datamodel.MonthlyRegionData;
 import it.uniroma2.dicii.sabd.covidproject.datamodel.TrendCoefficientRegion;
 import it.uniroma2.dicii.sabd.covidproject.utils.GlobalDataUtils;
 import it.uniroma2.dicii.sabd.covidproject.utils.TrendLineMonthlyRegionComparator;
+import org.apache.commons.math3.stat.StatUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -74,8 +75,85 @@ public class Query3 {
         KMeansModel model = kMeans.fit(featuredDF);
         Dataset<Row> clusteredRegions = model.transform(featuredDF);
         // Save clustering results in a CSV file
-        clusteredRegions.select("regionName","prediction")
+        clusteredRegions.select("prediction","regionName","trendLineCoefficient")
                 .write().format("csv").option("header", "false").save(outputDirectory);
+    }
+
+    public static Tuple2<Integer, TrendCoefficientRegion> assignRegionToCluster(Tuple2<Double,String> trendRegion, Double[] centroids) {
+        int minDistanceCentroid = 0;
+        double minDistance = Double.MAX_VALUE;
+        for (int i = 0; i < centroids.length; i++) {
+            double distance = Math.abs(centroids[i] - trendRegion._1);
+            if (distance < minDistance) {
+                minDistance = distance;
+                minDistanceCentroid = i;
+            }
+        }
+        return new Tuple2<>(minDistanceCentroid, new TrendCoefficientRegion(trendRegion._1, trendRegion._2));
+    }
+
+    public static Double computeClusterCentroid(Iterable<TrendCoefficientRegion> clusterPoints) {
+        double sum = 0.0;
+        int count = 0;
+        for (TrendCoefficientRegion tcr : clusterPoints) {
+            sum += tcr.getTrendLineCoefficient();
+            count += 1;
+        }
+        return  sum / count;
+    }
+
+    public static Double computeEuclideanDistance(Double[] a1, Double[] a2) {
+      double sum = 0.0;
+      for (int i = 0; i < a1.length; i++) {
+          sum += (a1[i]-a2[i])*(a1[i]-a2[i]);
+      }
+      return Math.sqrt(sum);
+    }
+
+    public static Double[] fromDoubleListToDoubleArray(List<Double> list) {
+        Double[] array = new Double[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+    }
+
+    // TODO review caching
+    public static void kMeansNaive(JavaPairRDD<Double,String> trendRegionPairs, String outputDirectory) {
+        // TODO ? make K,MAX_ITER,PRECISION,SEED configurable ?
+        List<Double> trendCoefficients = trendRegionPairs.map(x -> x._1).distinct().collect();
+        double[] trendCoefficientsArray = new double[trendCoefficients.size()];
+        for (int i = 0; i < trendCoefficients.size(); i++) {
+            trendCoefficientsArray[i] = trendCoefficients.get(i);
+        }
+        double firstCentroid = StatUtils.percentile(trendCoefficientsArray, (float)1/8 * 100);
+        System.out.println(firstCentroid);
+        double secondCentroid = StatUtils.percentile(trendCoefficientsArray, (float)3/8 * 100);
+        System.out.println(secondCentroid);
+        double thirdCentroid = StatUtils.percentile(trendCoefficientsArray, (float)5/8 * 100);
+        System.out.println(thirdCentroid);
+        double fourthCentroid = StatUtils.percentile(trendCoefficientsArray, (float)7/8 * 100);
+        System.out.println(fourthCentroid);
+        Double[] newCentroids = {firstCentroid, secondCentroid, thirdCentroid, fourthCentroid};
+        JavaPairRDD<Integer, TrendCoefficientRegion> clusteredTrendCoefficientRegions = null;
+        for (int i = 0; i < 20; i++) {
+            Double[] centroids = newCentroids;
+            clusteredTrendCoefficientRegions = trendRegionPairs.mapToPair(x -> assignRegionToCluster(x, centroids));
+            clusteredTrendCoefficientRegions.foreach(x -> {
+                System.out.println(x._1 + "," + x._2.getRegionName());
+            });
+            JavaPairRDD<Integer, Iterable<TrendCoefficientRegion>> trendRegionsByCluster = clusteredTrendCoefficientRegions.groupByKey();
+            // TODO caching?
+            newCentroids = fromDoubleListToDoubleArray(trendRegionsByCluster
+                    .mapValues(Query3::computeClusterCentroid)
+                    .sortByKey().map(x -> x._2).collect());
+            Double distanceFromOlderCentroids = computeEuclideanDistance(centroids, newCentroids);
+            if (distanceFromOlderCentroids < 0.0001) {
+                break;
+            }
+        }
+        clusteredTrendCoefficientRegions.map(x -> x._1 + "," + x._2.getRegionName()).saveAsTextFile(outputDirectory);
+
     }
 
 
@@ -114,13 +192,12 @@ public class Query3 {
             List<Tuple2<Double, String>> top50MonthlyAffectedRegion = kthMonthRegionData
                     .mapToPair(x -> new Tuple2<>(x._2.getTrendLineCoefficient(), x._2.getName()))
                     .top(50, new TrendLineMonthlyRegionComparator());
-
+            // TODO replace pair with object
             JavaPairRDD<Double, String> top50MonthlyAffectedRegionRDD =
                     sc.parallelizePairs(top50MonthlyAffectedRegion).cache();
 
             if (mode.equals("naive")) {
-                System.out.println("To be implemented");
-                // TODO invoke naive implementation of k-means
+                kMeansNaive(top50MonthlyAffectedRegionRDD, args[1] + "/month" + monthIndex);
             } else {
                 kMeansMLib(sc, top50MonthlyAffectedRegionRDD, args[1] + "/month" + monthIndex);
             }
