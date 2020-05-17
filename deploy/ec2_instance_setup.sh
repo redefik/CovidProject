c71f903ddaae9e7ad255c4c6797f14db624298cd
redefik <fe.viglietta@gmail.com>
2020-05-17 17:10:21 +0200

    EC2 instance setup

---
+++ deploy/ec2_instance_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script installs git and docker on an EC2 instance (Linux AMI)
+sudo yum update -y
+sudo yum install git -y
+sudo yum install docker -y
+sudo service docker start
+# Make docker usable without sudoing
+sudo usermod -a -G docker ec2-user
+echo "Please, exit and re-login to make changes effective"
+
